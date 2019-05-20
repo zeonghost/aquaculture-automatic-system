d53ae50c1efbe99b27dc68c3bc270372ce3556da
Michael Cloyd Lazaro <yuna5566@gmail.com>
2019-05-21 05:10:23 +0800

    Major Changes: Significant revisions in HomeActivity Removed old layout and replaced with a recyclerview + cardview layouts Logic is significantly changed Added Pond Class Added PondViewHolder Class
    Minor Changes:
    Created and moved Class Objects to a Model Package (Look at it as folder)
    Created a package for ViewHolders

---
+++ app/src/main/java/com/example/aquaculture/ViewHolder/PondViewHolder.java
@@ -0,0 +1,21 @@
+package com.example.aquaculture.ViewHolder;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.aquaculture.R;
+
+public class PondViewHolder extends RecyclerView.ViewHolder{
+    public TextView piId;
+    public TextView pondName;
+    public TextView location;
+
+    public PondViewHolder(@NonNull View itemView) {
+        super(itemView);
+        piId = itemView.findViewById(R.id.txtViewPiID);
+        pondName = itemView.findViewById(R.id.txtViewPondName);
+        location = itemView.findViewById(R.id.txtViewPondLocation);
+    }
+}
