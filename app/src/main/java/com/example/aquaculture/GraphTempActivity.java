ba034f53e1f275bb00bd7aceaac3698f693d2580
Tong Tiantai <wyexttt@163.com>
2019-05-28 11:09:22 +0800

    Add buttom navi in graph page

--- app/src/main/java/com/example/aquaculture/GraphTempActivity.java
+++ app/src/main/java/com/example/aquaculture/GraphTempActivity.java
@@ -1,12 +1,15 @@
 package com.example.aquaculture;
 
 import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.graphics.Color;
 import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
@@ -66,6 +69,7 @@ public class GraphTempActivity extends AppCompatActivity implements OnChartGestu
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graph_temp);
+        buttonNavigationSettings();
 
         getSupportActionBar().hide();
 
@@ -93,6 +97,30 @@ public class GraphTempActivity extends AppCompatActivity implements OnChartGestu
         lineChart.getLegend().setEnabled(false);
         lineChart.setExtraOffsets(5, 0, 5, 10);
         startingGraph();
+    }
+
+    private void buttonNavigationSettings(){
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.item1://btn 1 -> HomeActivity
+                        Intent intent1 = new Intent(GraphTempActivity.this, HomeActivity.class);
+                        startActivity(intent1);
+                        break;
+                    case R.id.item2://btn 2 -> task
+                        Intent intent2 = new Intent(GraphTempActivity.this, TaskActivity.class);
+                        startActivity(intent2);
+                        break;
+                    case R.id.item3://btn 3 -> profile
+                        Intent intent3 = new Intent(GraphTempActivity.this, ProfileActivity.class);
+                        startActivity(intent3);
+                        break;
+                }
+                return false;
+            }
+        });//bottom navigation
     }
 
     public class myXValueFormatter implements IAxisValueFormatter {
