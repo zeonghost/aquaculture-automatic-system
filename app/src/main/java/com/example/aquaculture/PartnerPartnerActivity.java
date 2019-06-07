69291d06d77cb4c8e6eebb3e43e50e5d82d655e1
Michael Cloyd Lazaro <yuna5566@gmail.com>
2019-06-08 02:09:48 +0800

    Just updating branch with recent layouts.

---
+++ app/src/main/java/com/example/aquaculture/PartnerPartnerActivity.java
@@ -0,0 +1,13 @@
+package com.example.aquaculture;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PartnerPartnerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_partner_partner);
+    }
+}
